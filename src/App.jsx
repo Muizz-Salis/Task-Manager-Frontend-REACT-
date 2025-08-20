0bfe217664b6cb4e5ecbe1ef4ca717ef2b57e5b2
Muizz-Salis <muizzadeyanju@gmail.com>
2025-08-20 16:18:09 +0100

    Frontend commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Tasks from './Tasks'
+import { Routes, Route } from 'react-router-dom'
+import Signup from './Signup'
+import Taskuu from './Taskuu'
+import Signin from './Signin'
+
+const App = () => {
+  return (
+    <>
+    <Routes>
+    <Route path="/tasks" element={<Tasks/>}/>
+    <Route path="/signup" element={<Signup/>}/>
+    <Route path="/tasku" element={<Taskuu/>}/>
+    <Route path="/signin" element={<Signin/>}/>
+    </Routes>
+    </>
+  )
+}
+
+export default App
